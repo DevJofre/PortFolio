928039671a3083f07e0d60bad3f29c321ece0dd8
Jofre Tomas <jofretomas9@gmail.com>
2024-10-29 17:38:05 -0300

    NavBar costruida

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import NavBar from "./component/Navbar"
+
 const App = () => {
   return (
-    <div>
-      <h1>
-        Hello mundo!
-      </h1>
+    <div className="overflow-x-hidden text-stone-300 antialiased">
+      <div className="fixed inset-0 -z-10">
+        <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
+      </div>
+
+      <div className="container mx-auto px-8">
+        <NavBar/>
+      </div>
     </div>
+    
   )
 }
 
---
+++ src/component/Navbar.jsx
@@ -0,0 +1,41 @@
+import logo from "../assets/raviKumarLogo.webp"
+import { FaLinkedin } from "react-icons/fa";
+import { FaGit } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+
+
+const NavBar = () => {
+    return(
+        <nav className="flex itens-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <a href="/" aria-label="Home">
+                    <img src={logo} className="mx-2" width={50} height={33} alt="Logo" />
+                </a>
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+
+                <a href="https://www.linkedin.com/in/jofre-tomas-811113197/"       
+                    target="_blank" 
+                    rel="nooperner noreferrer" 
+                    aria-label="LinkedIn">
+                    <FaLinkedin />
+                </a>
+                <a href="https://github.com/DevJofre" 
+                    target="_blank" 
+                    rel="nooperner noreferrer" 
+                    aria-label="GitHub">
+                    <FaGit />
+                </a>
+                <a href="https://www.instagram.com/joffrey_thomas/" 
+                    target="_blank" 
+                    rel="nooperner noreferrer" 
+                    aria-label="Instagram">
+                    <FaInstagram />
+                </a>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
+
