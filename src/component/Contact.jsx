3024954a37eaba741d93fb34f5dcd77843558471
Jofre Tomas <jofretomas9@gmail.com>
2024-11-04 20:17:46 -0300

    Animaçoes dos contatos finalizado

--- src/component/Contact.jsx
+++ src/component/Contact.jsx
@@ -1,13 +1,14 @@
+import { motion } from "framer-motion"
 import { CONTACT } from "."
 
 const Contact = () => {
     return (
       <div className=" border-t border-stone-900 pb-20">
-        <h2 className=" my-10 text-center text-4xl " > Entre em contato </h2>
+        <motion.h2 whileInView={{opacity: 1, y: 0}} initial={{opacity: 0, y:-100 }} transition={{duration: 0.5}} className=" my-10 text-center text-4xl " > Entre em contato </motion.h2>
         <div className="text-center tracking-tighter" >
-            <p className="my-4" >
+            <motion.p whileInView={{opacity: 1, x: 0}} initial={{opacity: 0, x:100 }} transition={{duration: 1}} className="my-4" >
                 {CONTACT.phoneNo}
-            </p>
+            </motion.p>
             <a href="a" className="border-b" >
             {CONTACT.email}
             </a>
